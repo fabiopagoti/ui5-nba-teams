e714a21fe33fe0d346e1e000a65c5fe6d55add7d
Fabio Pagoti <fabiopagoti@gmail.com>
2020-07-22 21:01:50 -0300

    list with teams

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,7 +7,9 @@ sap.ui.define(
 
 		return Controller.extend("ovly.app.controller.View1", {
 			onInit: function () {
-
+				let sURL = 'https://www.balldontlie.io/api/v1/teams';
+				let oTeams = new sap.ui.model.json.JSONModel(sURL);
+				this.getView().setModel(oTeams);
 			},
 		});
 	}
